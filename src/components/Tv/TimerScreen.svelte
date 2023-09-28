1489bec803d3a14c0da150256882ae96625e2f3e
valike23 <ludike23@gmail.com>
2023-09-28 14:54:51 +0100

    Update the timer component to use the new EvrScreen enum

--- src/components/Tv/TimerScreen.svelte
+++ src/components/Tv/TimerScreen.svelte
@@ -3,22 +3,72 @@
 
     export let match: Imatch = {};
     enum EvrScreen {
-        NONE = 'none',
-        RED = 'red-flag',
-        BLUE = 'blue-flag'
-
+        NONE = "none",
+        RED = "red-flag",
+        BLUE = "blue-flag",
     }
-    let vr= EvrScreen.NONE;
+    let vr = EvrScreen.NONE;
+    let startTime;
+    let countDownMilliSecond = 90000;
+    let timerInterval;
+    let minute = "00";
+    let second = "00";
+    let milli = "00";
+
+    function startTimer() {
+        console.log("the world war 2");
+        startTime = Date.now();
+        timerInterval = setInterval(updateTimer, 10); // Update every 10 milliseconds
+    }
+
+    function stopTimer() {
+        clearInterval(timerInterval);
+    }
+
+    function resetTimer() {
+        clearInterval(timerInterval);
+        minute = "00";
+        second = "00";
+        milli = "00";
+    }
+    function addLeadingZero(number) {
+        // Check if the number is a unit (between 0 and 9)
+        if (number >= 0 && number <= 9) {
+            // Add a leading zero and convert it to a string
+            return "0" + number.toString();
+        } else {
+            // If it's not a unit, return the number as a string without modification
+            return number.toString();
+        }
+    }
+
+    function updateTimer() {
+        const elapsedTime = Date.now() - startTime;
+        milli = addLeadingZero(Math.floor((elapsedTime % 1000) / 10));
+        second = addLeadingZero( Math.floor((elapsedTime / 1000) % 60));
+        minute = addLeadingZero( Math.floor(elapsedTime / 1000 / 60));
+    }
+
+    function padNumber(num) {
+        return num.toString().padStart(2, "0");
+    }
+    startTimer();
 </script>
 
 <div class="main">
     <div class="row red">
         <div class="col-2">
             <div style="height: 40%;">
-                <img style="height: 100%;" src="{match.redAthlete.club.flag}" alt="" />
+                <img
+                    style="height: 100%;"
+                    src={match.redAthlete.club.flag}
+                    alt=""
+                />
             </div>
             <div style="height: 30%;color:white;margin-bottom: -20px">
-                <h2 style="font-size: 200%; font-weight:700">{match.redAthlete.club.shortname}</h2>
+                <h2 style="font-size: 200%; font-weight:700">
+                    {match.redAthlete.club.shortname}
+                </h2>
             </div>
 
             <div style="height: 25%;color:red">
@@ -28,10 +78,16 @@
 
         <div class="col-7">
             <div style="height: 40%; padding-top:15px">
-                <h2 class="name">{match.redAthlete.firstName + " " + match.redAthlete.lastName}</h2>
+                <h2 class="name">
+                    {match.redAthlete.firstName +
+                        " " +
+                        match.redAthlete.lastName}
+                </h2>
             </div>
             <div style="height: 25%;color:white;margin-top:-20px">
-                <p style="font-size: 150%; font-weight:400">{match.redAthlete.club.clubName}</p>
+                <p style="font-size: 150%; font-weight:400">
+                    {match.redAthlete.club.clubName}
+                </p>
             </div>
 
             <div class="row" style="height: 35%;color:red">
@@ -50,15 +106,17 @@
         <div class="col-2">
             <div class="row">
                 <div class="col-5">
-                   {#if vr == EvrScreen.RED}
+                    {#if vr == EvrScreen.RED}
                         <div class="card vr">VR</div>
-                   {/if}
+                    {/if}
                 </div>
                 <div class="col-3">
                     <div class="circle" style="border-color: red;" />
                 </div>
                 <div class="col-4">
-                    <span style="color: red;" class="score float-right">{match.redScore}</span>
+                    <span style="color: red;" class="score float-right"
+                        >{match.redScore}</span
+                    >
                 </div>
             </div>
         </div>
@@ -66,10 +124,16 @@
     <div class="row blue">
         <div class="col-2">
             <div style="height: 40%;">
-                <img style="height: 100%;" src="{match.blueAthlete.club.flag}" alt="" />
+                <img
+                    style="height: 100%;"
+                    src={match.blueAthlete.club.flag}
+                    alt=""
+                />
             </div>
             <div style="height: 30%;color:white;margin-bottom: -20px">
-                <h2 style="font-size: 200%; font-weight:700">{match.blueAthlete.club.shortname}</h2>
+                <h2 style="font-size: 200%; font-weight:700">
+                    {match.blueAthlete.club.shortname}
+                </h2>
             </div>
 
             <div style="height: 25%;color:blue">
@@ -79,10 +143,16 @@
 
         <div class="col-7">
             <div style="height: 40%; padding-top:15px">
-                <h2 class="name">{match.blueAthlete.firstName + " " + match.blueAthlete.lastName}</h2>
+                <h2 class="name">
+                    {match.blueAthlete.firstName +
+                        " " +
+                        match.blueAthlete.lastName}
+                </h2>
             </div>
             <div style="height: 25%;color:white;margin-top:-20px">
-                <p style="font-size: 150%; font-weight:400">{match.blueAthlete.club.clubName}</p>
+                <p style="font-size: 150%; font-weight:400">
+                    {match.blueAthlete.club.clubName}
+                </p>
             </div>
 
             <div class="row" style="height: 35%;color:blue">
@@ -102,14 +172,15 @@
             <div class="row">
                 <div class="col-5">
                     {#if vr == EvrScreen.BLUE}
-                    <div class="card vr">VR</div>
+                        <div class="card vr">VR</div>
                     {/if}
                 </div>
                 <div class="col-3">
                     <div class="circle" style="border-color: blue;" />
                 </div>
                 <div class="col-4">
-                    <span style="color: blue;" class="score float-right">{match.blueScore}</span
+                    <span style="color: blue;" class="score float-right"
+                        >{match.blueScore}</span
                     >
                 </div>
             </div>
@@ -119,12 +190,10 @@
         <div class="col-3">
             <p class="kata">Kusami 1</p>
         </div>
-        <div class="col-1">
-            <img src="" alt="" />
-        </div>
-        <div class="col-4 clock">
-            <span class="minute">0:</span><span class="second">00.</span><sup
-                class="milli">22</sup
+        
+        <div class="col-5 clock">
+            <span class="minute">{minute}:</span><span class="second">{second}.</span><sup
+                class="milli">{milli}</sup
             >
         </div>
         <div class="col-4 kata"><p>{match.category}</p></div>
@@ -137,14 +206,14 @@
         font-weight: 800;
         padding-bottom: 1rem;
     }
-    .second{
+    .second {
         font-size: 9rem;
     }
     .minute {
         font-size: 9rem;
     }
     .kata {
-        font-size: 3.2rem;
+        font-size: 2.5rem;
         font-weight: 400;
         line-height: 1.3cm;
     }
